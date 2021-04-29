98eb1a30a50a0f5a9e4450a73f9ea21a2623f65d
histalium <histalium@outlook.com>
2021-04-29 20:44:28 +0200

    feat: add players that can end turn

---
+++ SomeGame.Cli/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeGame.Cli
+{
+    public class Player
+    {
+        public event EventHandler TurnEnded;
+
+        public event EventHandler TurnStarted;
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public void EndTurn()
+        {
+            TurnEnded?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void SetOtherPlayer(Player player)
+        {
+            player.TurnEnded += OtherPlayerTurnEnded;
+        }
+
+        private void OtherPlayerTurnEnded(object sender, EventArgs e)
+        {
+            TurnStarted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ SomeGame.Cli/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SomeGame.Cli
+{
+    class Program
+    {
+        private static Player _currentPlayer;
+
+        static void Main()
+        {
+            var player1 = new Player("player 1");
+            var player2 = new Player("player 2");
+
+            player1.TurnStarted += PlayerTurnStarted;
+            player2.TurnStarted += PlayerTurnStarted;
+
+            player1.SetOtherPlayer(player2);
+            player2.SetOtherPlayer(player1);
+
+            _currentPlayer = player1;
+
+            Console.WriteLine($"{_currentPlayer.Name} is current player");
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line.Equals("end turn", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _currentPlayer.EndTurn();
+                }
+                else
+                {
+                    Console.WriteLine("invalid command");
+                }
+            }
+        }
+
+        private static void PlayerTurnStarted(object sender, EventArgs e)
+        {
+            _currentPlayer = sender as Player;
+            Console.WriteLine($"{_currentPlayer.Name} is current player");
+        }
+    }
+}
