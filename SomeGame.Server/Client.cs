163207027672d81a154662eaba1fd0ac4220fc2a
histalium <histalium@outlook.com>
2021-05-14 21:00:47 +0200

    feat: add network client

---
+++ SomeGame.Server/Client.cs
@@ -0,0 +1,42 @@
+﻿using SomeGame.Logic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeGame.Server
+{
+    internal class Client
+    {
+        private readonly StreamWriter _writer;
+        private readonly StreamReader _reader;
+
+        public Client(Stream stream, string name)
+        {
+            _writer = new StreamWriter(stream, Encoding.ASCII);
+            _reader = new StreamReader(stream, Encoding.ASCII);
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public void Run()
+        {
+            var line = _reader.ReadLine();
+            while (line is not null)
+            {
+                Console.WriteLine(line);
+                line = _reader.ReadLine();
+            }
+        }
+
+        public void StartGame(Game game)
+        {
+            _writer.WriteLine($"Player 1: {game.Player1.Name}");
+            _writer.WriteLine($"Player 2: {game.Player2.Name}");
+            _writer.Flush();
+        }
+    }
+}
---
+++ SomeGame.Server/Program.cs
@@ -0,0 +1,87 @@
+﻿using SomeGame.Logic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SomeGame.Server
+{
+    class Program
+    {
+        private static List<Client> _clients = new();
+
+        static void Main(string[] args)
+        {
+            var port = 8080;
+            var listener = new TcpListener(IPAddress.Any, port);
+
+            listener.Start();
+            listener.BeginAcceptTcpClient(new AsyncCallback(ConnectClient), listener);
+
+            Console.WriteLine("Press any key to stop server");
+            Console.ReadKey();
+
+            listener.Stop();
+        }
+
+        private static void ConnectClient(IAsyncResult result)
+        {
+            var listener = (TcpListener)result.AsyncState;
+
+            var client = listener.EndAcceptTcpClient(result);
+            listener.BeginAcceptTcpClient(new AsyncCallback(ConnectClient), listener);
+
+            try
+            {
+                var ns = client.GetStream();
+                var reader = new StreamReader(ns, Encoding.ASCII);
+                var line = reader.ReadLine();
+                if (line.StartsWith("player ", StringComparison.OrdinalIgnoreCase))
+                {
+                    var player = line.Substring(7);
+                    var c = new Client(ns, player);
+
+                    var other = _clients
+                        .Where(t => t != c)
+                        .FirstOrDefault();
+
+                    if (other != null)
+                    {
+                        var card = new ResourceCard
+                        {
+                            Name = "Market card",
+                            Cost = new[] { new ResourceAmount { Amount = 2, Resource = new Resource { Id = "r1" } } }
+                        };
+                        var marketCards = Enumerable.Range(26, 30)
+                            .Select(t => card)
+                            .Cast<Card>()
+                            .ToList();
+
+                        var game = new Game(other.Name, marketCards, c.Name, marketCards);
+                        c.StartGame(game);
+                        other.StartGame(game);
+                    }
+
+                    _clients.Add(c);
+                    c.Run();
+                    _clients.Remove(c);
+                }
+                else
+                {
+                    var writer = new StreamWriter(ns, Encoding.ASCII);
+                    writer.WriteLine("400 invalid command");
+                    writer.Flush();
+                }
+
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
